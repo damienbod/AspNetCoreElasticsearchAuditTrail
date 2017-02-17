6165b2175bc16f56383a27d478f9cff3951a0cf9
damienbod <damien_bod@hotmail.com>
2017-02-17 10:32:57 +0100

    Adding a count

--- AuditTrail/AuditTrailProvider.cs
+++ AuditTrail/AuditTrailProvider.cs
@@ -9,10 +9,10 @@ namespace AuditTrail
 {
     public class AuditTrailProvider : IAuditTrailProvider
     {
-        private string indexName = $"auditlog-{DateTime.UtcNow.ToString("yyyy-MM-dd")}";
-        private const string alias = "auditlog";
-
-        public ElasticClient _elasticClient { get; }
+        private string _indexName = $"auditlog-{DateTime.UtcNow.ToString("yyyy-MM-dd")}";
+        private const string _alias = "auditlog";
+        private static Field TimestampField = new Field("timestamp");
+        private ElasticClient _elasticClient { get; }
 
         public AuditTrailProvider()
         {
@@ -20,7 +20,8 @@ namespace AuditTrail
             var connectionSettings = new ConnectionSettings(
                 pool,
                 new HttpConnection(),
-                new SerializerFactory((jsonSettings, nestSettings) => jsonSettings.Converters.Add(new StringEnumConverter())));
+                new SerializerFactory((jsonSettings, nestSettings) => jsonSettings.Converters.Add(new StringEnumConverter())))
+              .DisableDirectStreaming();
 
             _elasticClient = new ElasticClient(connectionSettings);
         }
@@ -29,7 +30,7 @@ namespace AuditTrail
         {
             var index = new IndexName()
             {
-                Name = indexName
+                Name = _indexName
             };
 
             var indexRequest = new IndexRequest<AuditTrailLog>(auditTrailLog, index);
@@ -41,15 +42,64 @@ namespace AuditTrail
             }
         }
 
-        public long Count(string filter)
+        public long Count(string filter = "*")
         {
-            throw new NotImplementedException();
+            EnsureAlias();
+            var searchRequest = new SearchRequest<AuditTrailLog>(Indices.Parse(_alias))
+            {
+                Size = 0,
+                Query = new QueryContainer(
+                    new SimpleQueryStringQuery
+                    {
+                        Query = filter
+                    }
+                ),
+                Sort = new List<ISort>
+                    {
+                        new SortField { Field = TimestampField, Order = SortOrder.Descending }
+                    }
+            };
+
+            var searchResponse = _elasticClient.Search<AuditTrailLog>(searchRequest);
+
+            return searchResponse.Total;
         }
 
         public IEnumerable<AuditTrailLog> SelectItems(string filter, AuditTrailPaging auditTrailPaging = null)
         {
+            EnsureAlias();
             throw new NotImplementedException();
         }
 
+        private void CreateAliasForAllIndices()
+        {
+            var response = _elasticClient.AliasExists(new AliasExistsRequest(new Names(new List<string> { _alias })));
+            if (!response.IsValid)
+            {
+                throw response.OriginalException;
+            }
+
+            if (response.Exists)
+            {
+                _elasticClient.DeleteAlias(new DeleteAliasRequest(Indices.Parse("auditlog-*"), _alias));
+            }
+
+            var responseCreateIndex = _elasticClient.PutAlias(new PutAliasRequest(Indices.Parse("auditlog-*"), _alias));
+
+            if (!responseCreateIndex.IsValid)
+            {
+                throw response.OriginalException;
+            }
+        }
+
+        private static DateTime aliasUpdated = DateTime.UtcNow.AddYears(-50);
+        private void EnsureAlias()
+        {
+            if (aliasUpdated.Date < DateTime.UtcNow.AddDays(-1).Date)
+            {
+                aliasUpdated = DateTime.UtcNow;
+                CreateAliasForAllIndices();
+            }
+        }
     }
 }
