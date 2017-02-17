1f464ba0f08eec3503f2c287702df13b0c2cdd82
damienbod <damien_bod@hotmail.com>
2017-02-17 09:30:42 +0100

    Adding ASP.NET Core MVC project

---
+++ AspNetCoreElasticsearchNestAuditTrail/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AuditTrail;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreElasticsearchNestAuditTrail.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAuditTrailProvider _auditTrailProvider;
+
+        public HomeController(IAuditTrailProvider auditTrailProvider)
+        {
+            _auditTrailProvider = auditTrailProvider;
+        }
+
+        public IActionResult Index()
+        {
+            // _auditTrailProvider.AddLog()
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
