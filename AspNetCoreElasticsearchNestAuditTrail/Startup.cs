5d17ab971a5adb26745f19cb38b03af9e989d659
damienbod <damien_bod@hotmail.com>
2020-01-12 09:48:56 +0100

    Updating to .NET  Core 3.1

--- AspNetCoreElasticsearchNestAuditTrail/Startup.cs
+++ AspNetCoreElasticsearchNestAuditTrail/Startup.cs
@@ -25,7 +25,8 @@ namespace AspNetCoreElasticsearchNestAuditTrail
                 options.UseSettings(indexPerMonth, amountOfPreviousIndicesUsedInAlias)
             );
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
